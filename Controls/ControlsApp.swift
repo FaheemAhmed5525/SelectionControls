6a499f5788d41a934f0a9df1750ac0c32a3ed9ce
Faheem Ahmed <faheemahmed5525@gmail.com>
2024-09-09 12:39:34 +0500

    Initial Commit

---
+++ Controls/ControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlsApp.swift
+//  Controls
+//
+//  Created by Faheeam Ahmed on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
